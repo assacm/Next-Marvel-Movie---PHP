1cd4dda59d5de259e68071d3e4b70a269a40850b
assacm <alma_julissa@live.com.mx>
2025-01-30 19:00:35 -0700

    Métodos estáticos en una clase

--- classes.php
+++ classes.php
@@ -1,35 +1,86 @@
 <?php
 
+declare(strict_types =1);
 
 class SuperHero{
 
-  //Propiedades y métodos
-    public $name;
-    public $powers;
-    public $planet;
-    
-    public function __construct($name, $powers, $planet) {
-        $this->name = $name;
-        $this->powers = $powers;
-        $this->planet = $planet;
+    //promoted properties PHP 8:
+    public function __construct(
+        readonly public string $name, //no se puede modificar, requiere especificar el tipo de dato
+        public array $powers, 
+        public $planet
+    ){}
+
+    public function show_all(){
+        return get_object_vars($this);
     }
 
-    
     public function attack(){
         return "¡$this->name ataca con sus poderes!";
     }
 
     public function description(){
+
+        $powers = implode(",", $this->powers);
         return "$this->name es un superhéroe nivel que viene de 
          $this->planet y tiene los siguientes poderes:
-         $this->powers 
+         $powers 
         ";
     }
+
+    public static function random(){
+        $names =["Thor","Spiderman", "Wolverine", "Ironman", "Hulk"];
+
+        $powers =[
+            ["Superfuerza", "Volar", "Rayos Láser"],
+            ["Superfuerza", "Super agilidad", "Telarañas"],
+            ["Regeneración", "Superfuerza", "Garras de adamantium"],
+            ["Superfuerza", "Volar", "Rayos láser"],
+            ["Superfuerza", "Super agilidad", "Cambio de tamaño"]
+
+        ];
+
+        $planets = ["Asgard", "HulkWorld", "Tierra", "krypton"];
+
+        $name = $names[array_rand($names)];
+        $power = $powers[array_rand($powers)];
+        $planet = $planets[array_rand($planets)]; //array_rand retorna una llave random de un array
+   
+        echo "El superhéroe elegido es $name, que viene de $planet y tiene los siguientes poderes: " . implode(",", $power);
+    }
+
+    public static function random_hero(){
+        $names =["Thor","Spiderman", "Wolverine", "Ironman", "Hulk"];
+
+        $powers =[
+            ["Superfuerza", "Volar", "Rayos Láser"],
+            ["Superfuerza", "Super agilidad", "Telarañas"],
+            ["Regeneración", "Superfuerza", "Garras de adamantium"],
+            ["Superfuerza", "Volar", "Rayos láser"],
+            ["Superfuerza", "Super agilidad", "Cambio de tamaño"]
+
+        ];
+
+        $planets = ["Asgard", "HulkWorld", "Tierra", "krypton"];
+
+        $name = $names[array_rand($names)];
+        $power = $powers[array_rand($powers)];
+        $planet = $planets[array_rand($planets)]; //array_rand retorna una llave random de un array
+   
+        return new self($name, $power, $planet);
+    }
+
 }
 
-
-$hero = new SuperHero("Batman","inteligencia, fuerza, tenología","Gotham");
+$hero = new SuperHero("Batman",["inteligencia", "fuerza", "tenología"],"Gotham");
 echo $hero->description();
 
-$hero2 = new SuperHero("Superman","Superfuerza, super calzones rojos, rayos láser","Krypton");
+$hero2 = new SuperHero("Superman",["Superfuerza", "super calzones rojos", "rayos láser"],"Krypton");
 echo $hero2->description();
+
+var_dump($hero->show_all());
+
+SuperHero::random(); // se utiliza :: para métodos estáticos.
+
+$rhero = SuperHero::random_hero();
+$rhero->description();
